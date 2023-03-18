b81bf96bb4af0be0df1cf599d795e6d75cc2d989
Aryan Varma <107306635+aryanvar@users.noreply.github.com>
2023-03-18 12:50:53 +0530

    navbar initialized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    
+    <div className="navabar">
+        <h2 className='mx-8 font-bold absolute'>Network-Me</h2>
+        <ul className='flex justify-center '>
+            <li className='mx-2'>Home</li>
+            <li className='mx-2'>Features</li>
+            <li className='mx-2'>Events</li>
+            <li className='mx-2'>Testimonials</li>
+            
+        </ul>
+    </div>
     
     </>
   )
