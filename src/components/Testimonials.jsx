ea8c028af760e2305dffd912cc24a207b97b0e6b
Aryan Varma <107306635+aryanvar@users.noreply.github.com>
2023-03-19 04:02:27 +0530

    updated

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,21 +7,21 @@ const Testimonials = () => {
     <>
   <h1 className='font-bold text-3xl text-center'>Testimonials</h1>
     <div className="flex flex-wrap">
-    <div className="flex flex-col w-[280px] h-[290px] justify-center items-center userCard gap-4 p-2 rounded-lg m-auto mt-5 shadow-xl ">
-      <img src={images} alt="" srcset="" className='profile-img w-[120px] h-[120px]'/>
+    <div className="flex flex-col w-[280px] h-[290px] justify-center items-center userCard gap-4 p-2 rounded-lg m-auto mt-5 shadow-s ">
+      <img src={images} alt="" className='profile-img w-[120px] h-[120px]'/>
       <p className='text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab.</p>
       <strong className='text-xl'>Alok Singh</strong>
       <p className='text-lg'>Web Developer</p>
     </div>
     <div className="flex flex-col w-[280px] h-[290px] justify-center items-center userCard gap-4 p-2 rounded-lg m-auto mt-5 shadow-xl ">
-      <img src={images} alt="" srcset="" className='profile-img w-[120px] h-[120px]'/>
-      <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+      <img src={images} alt="" className='profile-img w-[120px] h-[120px]'/>
+      <p className='text-center text-gray-400' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
       </p>
       <strong className='text-xl'>Alok Singh</strong>
       <p className='text-lg'>Web Developer</p>
     </div>
     <div className="flex flex-col w-[280px] h-[290px] justify-center items-center userCard gap-4 p-2 rounded-lg m-auto mt-5 shadow-xl ">
-      <img src={images} alt="" srcset="" className='profile-img w-[120px] h-[120px]'/>
+      <img src={images} alt="" className='profile-img w-[120px] h-[120px]'/>
       <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia.</p>
       <strong className='text-xl'>Alok Singh</strong>
       <p className='text-lg'>Web Developer</p>
