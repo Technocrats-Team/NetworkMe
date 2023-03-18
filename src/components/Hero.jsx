8aec840539cfbd9dfd2a4b7bd6394719992a70a2
Aryan Varma <107306635+aryanvar@users.noreply.github.com>
2023-03-18 15:02:21 +0530

    home page added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+    <div className="about">
+      <p className='pl-28 pt-32 text-4xl'>
+        Networking made easy with
+      </p>
+      <p className='pl-52 text-4xl text-indigo-400'>Network-me</p>
+      <p className='text-justify'>
+      Connect with like-minded professionals and expand your network.
+    </p>
+    <div className='flex mx-auto align-top'>
+    <button className='btn mx-4 border border-gray-500 px-4 py-2 rounded-lg'>Host</button>
+    <button className='btn mx-4 border border-gray-500 px-4 py-2 rounded-lg'>Attend</button>
+    </div>
+    </div>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-
+import React from "react";
+// import Logo from '../assets'
 const Navbar = () => {
   return (
     <>
-    <div className="navabar">
-        <h2 className='mx-8 font-bold absolute'>Network-Me</h2>
-        <ul className='flex justify-center '>
-            <li className='mx-2'>Home</li>
-            <li className='mx-2'>Features</li>
-            <li className='mx-2'>Events</li>
-            <li className='mx-2'>Testimonials</li>
-            
-        </ul>
-    </div>
+      <div className="navabar ">
+        
+          {/* <img src={Logo} alt=""  /> */}
+          <h2 className="mx-12 font-bold absolute text-2xl">Network-Me</h2>
+          <ul className="flex justify-center mt-3">
+            <li className="mx-4">Home</li>
+            <li className="mx-4">Features</li>
+            <li className="mx-4">Events</li>
+            <li className="mx-4">Testimonials</li>
+          </ul>
     
+      </div>
+          {/* <button
+            type="button"
+            class="inline-block rounded border-2 border-info px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-info transition duration-150 ease-in-out hover:border-info-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-info-600 focus:border-info-600 focus:text-info-600 focus:outline-none focus:ring-0 active:border-info-700 active:text-info-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+            data-te-ripple-init
+          >
+            Info
+          </button> */}
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
