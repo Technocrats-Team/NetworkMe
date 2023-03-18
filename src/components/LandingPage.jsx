d33c77a381ed4c6945ca7bb96bc722f887c562b9
Alok Singh <alok15120803@gmail.com>
2023-03-19 01:43:29 +0530

    auth

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import react , {useState} from 'react'
 import FeaturePage from '../components/FeaturePage.jsx'
 import Navbar from '../components/Navbar'
 
@@ -7,12 +7,23 @@ import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import EventPage from './EventPage.jsx'
 import AttendeeForm from './AttendeeForm.jsx'
+import {auth , provider } from '../firebase'
+import { signInWithPopup } from 'firebase/auth'
 
 const LandingPage = () => {
+  const [value , setValue] = useState();
+  const handleClick =() => {
+    signInWithPopup(auth , provider).then((data) => {
+       setValue(data.user.email)
+       console.log(value)
+    }
+    )
+  }
   return (
     <>
     <Navbar />
     <Hero />
+    
     <FeaturePage />
     <EventPage />
     <Testimonials />
