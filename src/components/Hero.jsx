4475078b206b97f2e3b794f84e6de4865bdd05a2
Alok Singh <alok15120803@gmail.com>
2023-03-19 04:45:50 +0530

    ..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,7 @@ const Hero = () => {
       <div className='svg-position '>
         <img src={team} alt="" className='' />
       </div>
-<<<<<<< HEAD
-    <div class="  space-x-32 mt-8 mx-52">
-=======
     <div className="  space-x-32 mt-8 mx-52">
->>>>>>> 13ea5f6700af3795bdd17b6c6460f44fa9e583a4
     <Link to="/register"> <button type="button"className="inline-block rounded bg-success px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)]">
     Host
   </button></Link>
