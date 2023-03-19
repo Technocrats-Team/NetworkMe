727ace4768ee1771a21324204c4132e2bcd057a5
Prathmesh Tangade <prathameshtangade097@gmail.com>
2023-03-19 10:12:01 +0530

    event form updated

--- src/components/EventRegistrationForm.jsx
+++ src/components/EventRegistrationForm.jsx
@@ -40,51 +40,51 @@ const EventRegistrationForm = () => {
         onSubmit={handleSubmit}
       >
         {({ values, handleSubmit, handleChange }) => (
-          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <form onSubmit={handleSubmit} className="w-[50vw] mx-auto ">
             <div className="flex flex-wrap gap-7 ">
             <label className="block mt-4">
             <span className="text-gray-700">Event Name:</span>
-              <Field type="text" name="eventName" className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w" />
+              <Field type="text" name="eventName" className="mt-1 field-text block w-52 h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w" />
             </label>
             <label className="block mt-4">
             <span className="text-gray-700">Event Date:</span>
-              <Field type="date" name="eventDate" className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" /> 
+              <Field type="date" name="eventDate" className="mt-1 field-text block w-52 h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" /> 
             </label>
             <label className='block mt-4'>
               Event Location:
-              <Field type="text" name="eventLocation" className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-2xl focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+              <Field type="text" name="eventLocation" className="mt-1 field-text block w-52 h-12 rounded-md border-[#988f8f] shadow-2xl focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
             </label>
             <label className='block mt-4'>
             <span className="text-gray-700">Event Description:</span>
-              <Field component="textarea" name="eventDescription" className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+              <Field component="textarea" name="eventDescription" className="mt-1 field-text block w-[45vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
             </label>
             </div>
             <FieldArray name="sponsors">
               {(arrayHelpers) => (
-                <div className='mt-4 '>
-                  <h3 className="text-lg font-medium">Sponsors:</h3>
+                <div className='mt-4 w-[50vw]'>
+                  <h3 className="text-lg font-medium text-center">Sponsors:</h3>
                   {values.sponsors.map((sponsor, index) => (
-                    <div key={index} className="mt-2">
+                    <div key={index} className="mt-2 w-[50vw]">
                       <label className="block">
                        <span> Name:</span>
                         <Field
                           type="text"
                           name={`sponsors.${index}.name`}
-                          className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
+                          className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
                       </label>
                       <label className=' block mt-4'>
                         Social Link:
                         <Field
                           type="text"
                           name={`sponsors.${index}.socialLink`}
-                          className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
+                          className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
                       </label>
                       <button
                         type="button"
                         onClick={() =>
                           arrayHelpers.remove(index)
                         }
-                        className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                        className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         Remove Sponsor
                       </button>
                     </div>
@@ -93,7 +93,7 @@ const EventRegistrationForm = () => {
                     type="button"
                     onClick={() =>
                       arrayHelpers.push({ name: '', socialLink: '' })
-                    } className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "
+                    } className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mx-auto "
                   >
                     Add Sponsor
                   </button>
@@ -103,7 +103,7 @@ const EventRegistrationForm = () => {
             <FieldArray name="organizers">
               {(arrayHelpers) => (
                 <div className=''>
-                  <h3>Organizers:</h3>
+                  <h3 className="mx-auto text-lg font-medium text-center">Organizers:</h3>
                   {values.organizers.map((organizer, index) => (
                     <div key={index}>
                       <label>
@@ -111,20 +111,20 @@ const EventRegistrationForm = () => {
                         <Field
                           type="text"
                           name={`organizers.${index}.name`}
-                          className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "/>
+                          className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "/>
                       </label>
                       <label>
                         Social Link:
                         <Field
                           type="text"
                           name={`organizers.${index}.socialLink`}
-                         className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "/>
+                         className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "/>
                       </label>
                       <button
                         type="button"
                         onClick={() =>
                           arrayHelpers.remove(index)
-                        }className=" mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "
+                        }className=" mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50  "
                       >
                         Remove Organizer
                       </button>
@@ -134,15 +134,15 @@ const EventRegistrationForm = () => {
                     type="button"
                     onClick={() =>
                       arrayHelpers.push({ name: '', socialLink: '' })
-                    } className="mt-1 field-text block w-52 h-7 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "
+                    } className="mt-1 field-text block w-[30vw] h-12 rounded-md border-[#988f8f] shadow-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mx-auto "
                   >
                     Add Organizer
                   </button>
                 </div>
               )}
             </FieldArray>
-            <button type="submit" className='btn  text-black font-bold py-8 px-4 m rounded'>Submit</button>
-            <Link to="/event"><button  className='btn  text-black font-bold py-8 px-4 m rounded'>Go to event Page</button></Link>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 align-center rounded-full w-[120px]">Submit</button>
+            <Link to="/event"><button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-full w-[120px]">Event Page</button></Link>
 
             
           </form>
